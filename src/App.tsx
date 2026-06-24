c31956b5d12b3cb5d8b3bc24ea37fd08f3465ae8
Mohd Azairi <azairi89@gmail.com>
2026-06-25 01:04:04 +0800

    UI improvement

--- src/App.tsx
+++ src/App.tsx
@@ -2231,7 +2231,7 @@ const tomorrowTCA = useMemo(() => {
 
 )}
 
-    <div className="min-h-screen bg-gradient-to-br from-slate-300 via-emerald-100 to-indigo-500 flex flex-col font-sans text-slate-900">
+    <div className="min-h-screen bg-gradient-to-br from-slate-300 via-slate-200 to-slate-300 flex flex-col font-sans text-slate-900">
       {/* Header */}
       <header className="bg-white border-b border-slate-200 sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-3 py-2 md:h-16 flex flex-col md:flex-row md:items-center justify-center md:justify-between gap-3 md:gap-4">
